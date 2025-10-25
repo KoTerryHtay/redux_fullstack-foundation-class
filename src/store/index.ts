672e36032dcd5f6f7d467baa607ade364ec7f3a9
KoTerryHtay <luaye1990@gmail.com>
2025-10-25 10:30:36 +0630

    Setup RTK query

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counterSlice";
 import postReducer from "./postsSlice";
+import { apiSlice } from "./rtk/apiSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     posts: postReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
+
+// optional, but recommended for refetchOnFocus/refetchOnReconnect behaviors
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
